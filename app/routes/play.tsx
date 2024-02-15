7adeaff8a8d58546ee2e28e2fa39b60dbce32d4d
Pietro Rhyan <pietroqjg@gmail.com>
2024-02-15 14:08:04 -0300

    refactor: indendation

--- app/routes/play.tsx
+++ app/routes/play.tsx
@@ -3,6 +3,7 @@ import { QuestionGroup } from "~/components/QuestionGroup";
 
 export default function Play() {
   const [questionGroupVoted, setQuestionGroupVoted] = useState(false)
+  
   function handleQuestionGroupVoted() {
     setQuestionGroupVoted(true)
   }
@@ -12,9 +13,28 @@ export default function Play() {
       <h2 className="text-xl text-white font-semibold mb-7">você escolheria</h2>
 
       <QuestionGroup>
-        <QuestionGroup.Item questionVariants="left" totalQuestionVotes={639} votes={394} showInfoWhenVoted={questionGroupVoted} hasVoted={handleQuestionGroupVoted} >Trabalhar 8 horas remoto todo dia</QuestionGroup.Item>
-        <QuestionGroup.Item questionVariants="right" totalQuestionVotes={639} votes={245} showInfoWhenVoted={questionGroupVoted} hasVoted={handleQuestionGroupVoted} >Trabalhar 8 horas presencial por 3 dias</QuestionGroup.Item>
+        <QuestionGroup.Item 
+          questionVariants="left" 
+          totalQuestionVotes={639} 
+          votes={394} 
+          showInfoWhenVoted={questionGroupVoted} 
+          hasVoted={handleQuestionGroupVoted}
+        >
+          Trabalhar 8 horas remoto todo dia
+        </QuestionGroup.Item>
+
+        <QuestionGroup.Item 
+          questionVariants="right" 
+          totalQuestionVotes={639} 
+          votes={245} 
+          showInfoWhenVoted={questionGroupVoted} 
+          hasVoted={handleQuestionGroupVoted} 
+        >
+          Trabalhar 8 horas presencial por 3 dias
+        </QuestionGroup.Item>
       </QuestionGroup>
+
+      
     </main>
   )
 }
